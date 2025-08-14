769814c4e24bf94fc006523da647f632daa68ab4
Andy Jakubowski <hello@andyjakubowski.com>
2025-08-14 11:08:21 +0200

    Add explanatory comment for pinning model revision
    Signed-off-by: Andy Jakubowski <hello@andyjakubowski.com>

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import torch
 from config import HF_MODEL_ID, HF_REVISION
 
 print("Model ID:", HF_MODEL_ID)
+# Pin the model revision to ensure reproducibility and prevent unexpected downloads once the model is in the HF cache
 print("Model revision: ", HF_REVISION)
-
 
 pipe = pipeline(
     "text-generation",
