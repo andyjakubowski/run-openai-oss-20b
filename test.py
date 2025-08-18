0c13cfeff9eabe180e88a61723882bff3ac15cfe
Andy Jakubowski <hello@andyjakubowski.com>
2025-08-18 13:20:04 +0200

    Rename `config` to `constants`
    Signed-off-by: Andy Jakubowski <hello@andyjakubowski.com>

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from transformers import pipeline
-from config import HF_MODEL_ID, HF_REVISION
+from constants import HF_MODEL_ID, HF_REVISION
 
 print("Model ID:", HF_MODEL_ID)
 # Pin the model revision to ensure reproducibility and prevent unexpected downloads once the model is in the HF cache
